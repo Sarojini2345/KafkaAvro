44e23c5ff3fc39526612134bf4c93ccb263060dd
Sarojini2345 <sarojinirath21712@gmail.com>
2024-10-13 02:01:33 +0530

    KafkaAvro schemaRegistryApp added.

---
+++ src/main/java/com/kafka/avro/controller/EventController.java
@@ -0,0 +1,21 @@
+package com.kafka.avro.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kafka.avro.dto.Employee;
+import com.kafka.avro.producer.KafkaAvroProducer;
+
+@RestController
+public class EventController {
+    @Autowired
+    private KafkaAvroProducer producer;
+
+    @PostMapping("/events")
+    public String sendMessage(@RequestBody Employee employee) {
+        producer.send(employee);
+        return "message published !";
+    }
+}
